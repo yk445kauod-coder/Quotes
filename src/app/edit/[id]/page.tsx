e4c9972fab7413ccb6fb2bd07e693b91175866d0
Yousef <yk445kauod@gmail.com>
2025-08-25 12:17:23 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/edit/[id]/page.tsx
+++ src/app/edit/[id]/page.tsx
@@ -1,5 +1,9 @@
 import { EditForm } from "./edit-form";
 
+// This tells Next.js to generate dynamic routes on demand
+// instead of returning a 404 if the page wasn't pre-built.
+export const dynamicParams = true;
+
 // This function is required for static export with dynamic routes.
 // It tells Next.js not to pre-render any specific pages at build time.
 // The client will handle rendering based on the dynamic ID.
