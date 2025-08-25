5d69970de9b04e349f6c30689d9f3414b1b8508c
Yousef <yk445kauod@gmail.com>
2025-08-25 13:10:44 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

---
+++ src/next.config.ts
@@ -0,0 +1,36 @@
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'ik.imagekit.io',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  devIndicators: {
+    allowedDevOrigins: [
+      'https://*.cloudworkstations.dev',
+      'https://*.firebase.studio',
+    ]
+  }
+};
+
+export default nextConfig;
